12dfb3ded23344d26ca50e19f0b86b71d002f83d
Alan Gomes Coimbra <algcoimbra01@gmail.com>
2018-11-05 21:05:19 -0200

    Special Move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Enter Piece for Promotion (B/N/R/Q): ");
+					String type = mostrar.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}catch(ChessException e) {
 				System.out.println(e.getMessage());
 				mostrar.nextLine();
